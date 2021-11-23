fb546249e02b4bc58f3e9131e96205d8ea5b7bd6
FoloraD <fduang@gmail.com>
2021-11-23 16:07:09 +0000

    Updated the computerPlay function

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 /**  function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. **/
 
-const computerPlay = ( ) => {
-    const randomNumber = (Math.floor(Math.random() *3))
-    if (randomNumber === 0) {
-        return 'Rock';
-    }
-    else if (randomNumber === 1) {
-        return 'Paper';
-    }
-    else if (randomNumber === 2) {
-        return 'Scissors'
-    }
-
-
+function computerPlay() {
+    const options = ['Rock', 'Paper', 'Scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return options [randomNumber];
 }
-console.log (computerPlay())
+  console.log(computerPlay() )
+    
+   
+  
+
+/** function that plays a single round of Rock Paper Scissors. **/
+/** The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" **/
+/** Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation). **/
