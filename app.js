790a12f78ffec0beab922faff55d7b22e0efdd7c
FoloraD <fduang@gmail.com>
2021-12-01 15:06:57 +0000

    added object function for game scores

--- app.js
+++ app.js
@@ -1,157 +1,124 @@
-//0.... create player and computer variables & Dom id's
-//Set computer & player scores to 0. These will get updated when playing the game.
-let playerScore = 0;
-let computerScore = 0;
+function localGame() {
+  //0.... create player and computer variables & Dom id's
+  //Set computer & player scores to 0. These will get updated when playing the game.
+  let playerScore = 0;
+  let computerScore = 0;
+  let round = 0;
 
-// Show on the DOM the Computer input / Player input /game result//
-const computerChoice = document.getElementById('computer-choice')
-const playerChoice = document.getElementById('player-choice')
-const resultsDisplay = document.getElementById('results')
+  const scores = { // object to keep game scores
+    player: 0,
+    computer: 0,
+  };
+  // Show on the DOM the Computer input / Player input /game result//
+  const computerChoice = document.getElementById("computer-choice");
+  const playerChoice = document.getElementById("player-choice");
+  const resultsDisplay = document.getElementById("results");
 
-//rock / paper / scissor/ reset buttons//
+  //Player selection options for rock/paper/scissors
 
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
-const resetBtn = document.getElementById("reset");
+  const rock = document.getElementById("rock");
+  const paper = document.getElementById("paper");
+  const scissors = document.getElementById("scissors");
+  const resetBtn = document.getElementById("reset");
+  const gameRound = document.getElementById("round");
 
-//function that computer randomly selects from array of ‘Rock’, ‘Paper’ or ‘Scissors’ and return a value. 
+  //function that computer randomly selects from array of ‘Rock’, ‘Paper’ or ‘Scissors’ and return a value.
 
-function computerPlay() {
-    const options = ['rock', 'paper', 'scissors'];
+  function computerPlay() {
+    const options = ["rock", "paper", "scissors"];
     const randomNumber = options[Math.floor(Math.random() * 3)]; //generates a random number between 0 - 2
     return randomNumber; // will return a random element from the array above
-} 
-// function to determine result of playing 1 round.
-function determineResult(playerSelection, computerSelection) {
+  }
+  // function to determine result of playing 1 round.
+  function determineResult(playerSelection, computerSelection) {
     console.log(`player selected: "${playerSelection}"`);
     console.log(`Computer selected: "${computerSelection}"`);
-   
+
     if (playerSelection === computerSelection) {
-        return ['draw', playerSelection, computerSelection]
+      return ["draw", playerSelection, computerSelection];
     }
-   
-    //winingCombos - stores the wining combinations in a nested array. 
+
+    //winingCombos - stores the wining combinations in a nested array.
     //Each nested array -[ [ 0 ~ player selection ], [1~ computer selection], [2 ~ winning item ]
-    const winningCombos = [ ['scissors', 'paper', 'scissors'], ['rock', 'scissors', 'rock'],  [ 'paper', 'rock', 'paper']  ] // each array contain the winning combos for the game. eg scissors beats paper, so scissors win
-    
+    const winningCombos = [
+      ["scissors", "paper", "scissors"],
+      ["rock", "scissors", "rock"],
+      ["paper", "rock", "paper"],
+    ]; // each array contain the winning combos for the game. eg scissors beats paper, so scissors win
+
     // For loop: start at [0][0] loop through the winningCombos arrays to compare player selection and computer selection. conditions set for when player wins or if computer wins.
     for (let index = 0; index < winningCombos.length; index++) {
-        if (winningCombos[index][0] === playerSelection && winningCombos[index][1] === computerSelection) {
-            return ['player', playerSelection, computerSelection ]  
-            //player is winner of this round
-        } 
-        if (winningCombos[index][1] === playerSelection && winningCombos[index][0] === computerSelection) {
-            return ['computer', computerSelection, playerSelection]
-            //computer is winner of this round
-        }
+      if (
+        winningCombos[index][0] === playerSelection &&
+        winningCombos[index][1] === computerSelection
+      ) {
+        return ["player", playerSelection, computerSelection];
+        //player is winner of this round
+      }
+      if (
+        winningCombos[index][1] === playerSelection &&
+        winningCombos[index][0] === computerSelection
+      ) {
+        return ["computer", computerSelection, playerSelection];
+        //computer is winner of this round
+      }
     }
-}
+  }
 
-//2.... function that plays a single round of Rock Paper Scissors
-function playRound (playerSelection, computerSelection) {
-  const result = determineResult(playerSelection, computerSelection)
-  console.log(result) // returns values in array
-
-  
-
-  //TO DO List 
-  // 1. write a function to keep score (0 -5)
-  //2. & improve on the below logic. less repetition and cleaner code
-  // use this `${result[0]} wins!, ${result[1]} beats ${result[2]}` // string interpolation for game out come.
-  // 3. add event listeners for game
-
- 
-
-//player win outcomes
-
- /**  Rock  =>  Beats Scissors => Loses to Paper
-    Paper =>  Beats Rock => Loses to Scissors
-    Scissors => Beats Paper => Loses to Rock **/
-
-     if (playerSelection === 'scissors' && computerSelection === 'paper') {  
-              
-                playerScore++;
-            updateResult('You win! scissors beats paper')     
-
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            
-            playerScore++;
-            updateResult('You win! Rock beats scissors')   
-
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-           
-           playerScore++;
-           updateResult('You win! Paper beats rocks')       
-
-//Draw outcomes
-  } else if (playerSelection === computerSelection) {
-            updateResult("It's a draw! Play again")
-
-//computer win outcomes
-}  else if ( computerSelection === 'scissors' && playerSelection === 'paper') {
-     
-            computerScore++;
-            updateResult('You loose! Scissors beats paper')
-
- } else if ( computerSelection === 'rock' && playerSelection === 'scissors') {
-    //return('You loose! Rock beats scissors');
-            computerScore++;
-            updateResult('You loose! Rock beats scissors');
-            
- }else if (computerSelection === 'paper' && playerSelection === 'rock') {
-    //return( 'You loose! Paper beats rock');
-            computerScore++;
-            updateResult('You loose! Paper beats rock')
-
- }
- 
- // function that updates the result on the game board
- function updateResult(resultString) { 
-    document.getElementById('result').innerHTML = resultString // adds the game outcome to the game board screen. on the results section.
-    document.getElementById('result').style.color ="red"; // update this section - colours for outcomes red-loose, green-win, black-draw
-    document.getElementById("player-choice").innerHTML = playerSelection // adds the players selection (r/p/s) to the game board
-    document.getElementById("computer-choice").innerHTML = computerSelection // adds the computer selection (r/p/s) to the game board
-    document.getElementById("computer-score").innerHTML = computerScore  // adds points to computer score counter
-    document.getElementById("player-score").innerHTML = playerScore   // adds points to player score counter
-}
-
-};
-
-// play a 5 round game & keep score up to five.
-function game(playerChoice) {    //event listener will fill in the playerChoice with rock/paper/scissors
-    let player = playerChoice;
-    let computer = computerPlay();
-
-        if (playerScore == 5) {
-        updateResult("you have won the game")
-    
-   
-    } else if (computerScore == 5) {
-           updateResult("The computer has won the game") 
-    } else {
-        playRound(player, computer)
+  //2.... function that plays 5 rounds of Rock Paper Scissors
+  function playRound(playerSelection) {
+    if (round === 5) {
+      // check if round is 5, if yes then exit game. update ui to say who won.
+      console.log("game over");
+      return;
     }
-    
+
+    const computerSelection = computerPlay();
+    const result = determineResult(playerSelection, computerSelection);
+    console.log(result); // returns values in array
+
+    if (result[0] === "draw") {
+      updateResult("It's a draw! Play again");
+      return;
+    }
+
+    round = round + 1; // awards a point to the winner of the round
+    document.getElementById("round").innerHTML = round; // only updates score when player or computer wins.
+
+    scores[result[0]] = scores[result[0]] + 1; //dynamic object
+    console.log(scores); //accessing the score from the round
+
+    updateResult(`${result[0]} wins!, ${result[1]} beats ${result[2]}`);
+  }
+
+  // function that updates the result on the game board
+  function updateResult(resultString) {
+    // document.getElementById('result').innerHTML = resultString // adds the game outcome to the game board screen. on the results section.
+    // document.getElementById('result').style.color ="red"; // update this section - colours for outcomes red-loose, green-win, black-draw
+    // document.getElementById("player-choice").innerHTML = playerSelection // adds the players selection (r/p/s) to the game board
+    // document.getElementById("computer-choice").innerHTML = computerSelection // adds the computer selection (r/p/s) to the game board
+    // document.getElementById("computer-score").innerHTML = scores.computer  // adds points to computer score counter. Using objects
+    // document.getElementById("player-score").innerHTML = scores.player  // adds points to player score counter. using objects
+    Object.keys(scores).forEach((key) => {
+      document.getElementById(`${key}-score`).innerHTML = scores[key]; // doing the same as lines 144/146 removing duplication
+    });
+  }
+
+  //click buttons event listener for when player selects a button for rock paper scissors
+
+  rock.addEventListener("click", () => {
+    playRound("rock");
+  });
+  paper.addEventListener("click", () => {
+    playRound("paper");
+  });
+  scissors.addEventListener("click", () => {
+    playRound("scissors");
+  });
+  //;
 }
+localGame();
 
-//click buttons event listener for when player selects a button for rock paper scissors
-
-rock.addEventListener('click', () => {
-    game("rock");
-});
-paper.addEventListener('click', () => {
-    game("paper");
-});
-scissors.addEventListener('click', () => {
-    game("scissors");
-});
-resetBtn.addEventListener('click', function () {
-    location.reload();
-});
-
-
-const playerSelection = game() // game function is player choice from buttons pressed
- const computerSelection = computerPlay() 
- console.log(playRound(playerSelection, computerSelection)) // play 1 round & shows result on console
- 
+// const startingPlayerSelection = game() // game function is player choice from buttons pressed
+//  const startingComputerSelection = computerPlay()
+//  console.log(playRound(startingPlayerSelection, startingComputerSelection)) // play 1 round & shows result on console
